f1a5935f58475d9794ffa45bc380038e9346b40c
AjayiMike <ajayimikeofficial@gmail.com>
2023-03-13 22:32:31 +0100

    try out put_flash

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,6 +2,10 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   def new(conn, _params) do
-    render(conn, :create_new_topic)
+    # json(conn, %{status: "success", message: "I don't know what to tell you"})
+    # render(conn, :create_new_topic)
+    conn
+    |> put_flash(:info, "Welcome to create a new discussion!")
+    |> render(:create_new_topic)
   end
 end
